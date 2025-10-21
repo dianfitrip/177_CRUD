699ec42819281dbc2f683ce65a7ebcd578d1a944
dianfitrip <dianfitri683@gmail.com>
2025-10-21 15:10:17 +0700

    koneksi ke database

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+let mysql = require("mysql2");
+const app = express();
+const PORT = 3000;
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+const db = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'Df999999999999.',
+  database: 'mahasiswa',
+  port: 3306
+});
+
+db.connect((err) => {
+    if (err) {
+      console.error('Error connecting to the database:' + err.stack);
+      return;
+    }
+    console.log('Koneksi Berhasil!');
+});
+
